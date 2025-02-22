59163d3c41e3648bca071e909162cabc4e049baa
daikaduc05 <danghoaiduc9@gmail.com>
2025-02-23 04:36:36 +0700

    Revert "again, you stupid"
    This reverts commit c0cb25093e6d8eb8cb2bbd4c3dcc9ae5642e03b3.

--- FE/booking_photo/src/app/admin/(dashboard)/package/page.tsx
+++ FE/booking_photo/src/app/admin/(dashboard)/package/page.tsx
@@ -20,7 +20,7 @@ import axios from "axios";
 
 const title = ["Tên gói", "Giá", "Mô tả"];
 
-const Page = () => {
+const page = () => {
   const [packageItem, setPackageItem] = useState<IPackagesAdmin[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [name, setName] = useState<string>("");
