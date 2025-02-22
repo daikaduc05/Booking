32fe288755a055323f168cd36628a4674e06c2f7
vo-huy-hoang <huyhoangdn2005@gmail.com>
2025-02-23 01:48:44 +0700

    fix select product + package

--- BE/Booking-Photo/src/main/java/org/example/bookingphoto/repository/IProductRepository.java
+++ BE/Booking-Photo/src/main/java/org/example/bookingphoto/repository/IProductRepository.java
@@ -10,9 +10,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface IProductRepository extends JpaRepository<Product, Integer> {
-    @Query(value = "SELECT new org.example.bookingphoto.dto.ProductShowDTO(pr.id, pr.image, p.id) from Product as pr left join Package as p on pr.packageField.id = p.id where p.id = :packageId")
+    @Query(value = "SELECT new org.example.bookingphoto.dto.ProductShowDTO(pr.id, pr.image, p.id, p.name, p.price, p.description) from Product as pr left join Package as p on pr.packageField.id = p.id where p.id = :packageId")
     List<ProductShowDTO> showProductByPackage(@Param("packageId") Integer packageId);
 
-    @Query(value = "SELECT new org.example.bookingphoto.dto.ProductShowDTO(pr.id, pr.image, p.id) from Product as pr left join Package as p on pr.packageField.id = p.id")
+    @Query(value = "SELECT new org.example.bookingphoto.dto.ProductShowDTO(pr.id, pr.image, p.id, p.name, p.price, p.description) from Product as pr left join Package as p on pr.packageField.id = p.id")
     List<ProductShowDTO> showProducts();
 }
