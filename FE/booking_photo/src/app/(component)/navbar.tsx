05099b47e0e66d83324e5643f0820e9749b2ed0c
Tranhthudjt <ahmobile17022005@gmail.com>
2025-03-03 00:02:29 +0700

    cccc

--- FE/booking_photo/src/app/(component)/navbar.tsx
+++ FE/booking_photo/src/app/(component)/navbar.tsx
@@ -6,7 +6,6 @@ import { useParams, useRouter } from "next/navigation";
 import Image from "next/image";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -44,24 +43,17 @@ const Navbar = () => {
   }, [router, language]);
 
   return (
-    <div className="">
+    <div className="bg-none">
       <div
-        className={`flex flex-col fixed z-10 bg-[#484848] px-10 w-screen top-0 shadow-lg transition-all duration-500 ease-in-out transform ${
-          isOpen
-            ? "max-h-[300px] opacity-100 translate-y-0 "
-            : "max-h-0 opacity-0 translate-y-[-100%]"
-        }`}
-        style={{
-          overflow: "hidden",
-        }}
+        className={`flex flex-col fixed z-10  px-10 w-screen top-0 underline-offset-1 transition-all duration-500 ease-in-out transform`}
       >
         <div className="flex flex-col lg:flex-row items-center justify-between">
-          <div className="bg-[#484848] pl-10 h-[100px] top-0 z-50 flex flex-col lg:flex-row items-center">
+          <div className=" pl-10 h-[100px] top-0 z-50 flex flex-col lg:flex-row items-center">
             {navbar.map((item, index) => (
               <div
                 onClick={() => scrollToSection(item.id)}
                 key={index}
-                className="h-full w-[160px] duration-300 cursor-pointer hover:bg-[#D9D9D9] hover:text-black text-white flex items-center justify-center"
+                className="h-full w-[160px] duration-300 cursor-pointer hover:underline text-white flex items-center justify-center"
               >
                 {t(`${item.title}`)}
               </div>
@@ -88,42 +80,21 @@ const Navbar = () => {
               <select
                 value={language}
                 onChange={(e) => setLanguage(e.target.value)}
-                className="bg-[#484848] text-white border-none outline-none transition-all ease-in-out cursor-pointer duration-300 hover:text-[#cfcece]"
+                className="text-white border-none outline-none transition-all ease-in-out cursor-pointer duration-300 hover:text-[#cfcece]"
               >
                 <option value="vn">VN</option>
                 <option value="en">EN</option>
               </select>
             </div>
-
-            <button
+            {/* <button
               onClick={() => scrollToSection("packages")}
               className="w-[120px] h-[50px] rounded-lg bg-[#c4c4c4] hover:bg-[#b0aeae] shadow-lg duration-300"
             >
               {t("book")}
-            </button>
+            </button> */}
           </div>
         </div>
-        <button
-          className={`fixed top-5 left-10 z-50 mt-5 mr-5 text-white hover:text-[#d6d5d5] duration-300 ${
-            !isOpen && "hidden"
-          }`}
-          onClick={() => setIsOpen(false)}
-        >
-          <FaArrowCircleUp className="size-5" />
-        </button>
       </div>
-
-      {!isOpen && (
-        <div>
-          <button
-            className="fixed top-5 left-8 opacity-70 z-50 mt-5 mr-5 flex font-bold gap-2 items-center px-2 rounded-xl text-black  hover:bg-white hover:opacity-100 duration-300"
-            onClick={() => setIsOpen(true)}
-          >
-            {t("menu")}
-            <FaArrowCircleDown className="size-5" />
-          </button>
-        </div>
-      )}
     </div>
   );
 };
